ce096fa947ed5d45a0144fd76509aed220761380
Alyona8891 <besssta888@mail.ru>
2023-04-15 13:40:10 +0300

    feat: add my solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -19,31 +19,25 @@ function transform(arr) {
     throw new Error ("'arr' parameter must be an instance of the Array!")
   }
   const newArr = [].concat(arr);
+  const result = [];
   newArr.forEach((el, index) => {
     if(el === '--discard-next' && index !== newArr.length - 1) {
-      newArr.splice(index, 2);
-    } else if (el === '--discard-next' && index === newArr.length - 1) {
-      newArr.splice(index, 1);
+      newArr.splice(index + 1, 1);
     }
-    if(el === '--discard-prev' && index !== 0) {
-      newArr.splice(index - 1, 2);
-    } else if (el === '--discard-prev' && index === 0) {
-      newArr.splice(index, 1);
-    }
+    if(el === '--discard-prev' && index !== 0 && newArr[index-1] !== '--discard-next') {
+      result.pop();
+    } 
     if (el === '--double-next' && index !== newArr.length - 1) {
-      el = newArr[index+1];
-    } else if (el === '--double-next' && index === newArr.length - 1) {
-      newArr.splice(index, 1);
+      result.push(newArr[index+1]);
     }
-    if ((el === '--double-prev' && index !== 0) && (el === '--double-prev' && arr[index-2] !== '--discard-next')) {
-      el = newArr[index-1];
-    } else if (el === '--double-prev' && index === 0) {
-      newArr.splice(index, 1);
-    } else if (el === '--double-prev' && arr[index] === '--discard-next') {
-      newArr.splice(index, 1);
+    if (el === '--double-prev' && index !== 0 && newArr[index-1] !== '--discard-next') {
+      result.push(newArr[index-1]);
+    } 
+    if (el !== '--double-prev' && el !== '--double-next' && el !== '--discard-next' && el !== '--discard-prev') {
+      result.push(el);
     }
   })
- return newArr;
+ return result;
 }
 
 module.exports = {
